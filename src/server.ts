218e9dfddfb5f0a2d72c9142d48657620a740c50
Bruno Girome <brunogirome@gmail.com>
2026-02-14 21:09:24 -0300

    eslint and pretter setup, also npmrc

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { app } from './app.js';
+
+const host = '0.0.0.0';
+const port = 3333;
+
+app
+  .listen({ host, port })
+  .then(() => console.log(`🏋️ Server running | Address: ${host}:${port}`));
