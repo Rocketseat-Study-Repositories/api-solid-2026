d01b201bedcf514510710504ac47f43958741013
Bruno Girome <brunogirome@gmail.com>
2026-02-16 13:29:12 -0300

    services/use-case sample

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
+import { registerUseCase } from '@/services/register.js';
 import { FastifyReply, FastifyRequest } from 'fastify';
-import { hash } from 'bcryptjs';
-import { z } from 'zod';
 
-import { prisma } from '@/lib/prisma.js';
+import { z } from 'zod';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registrationBody = z.object({
@@ -13,17 +12,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registrationBody.parse(request.body);
 
-  const password_hash = await hash(password, 5);
-
-  const userWithSameEmail = await prisma.user.findUnique({ where: { email } });
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
     return reply.status(409).send();
   }
-
-  await prisma.user.create({
-    data: { name, email, password_hash },
-  });
 
   return reply.status(201).send();
 }
